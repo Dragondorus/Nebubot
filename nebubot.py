ae3e2ffbfb486c95c5a8b7a38cb91de6cf2af46e
allan dandrieux <allan.dandrieux@epitech.eu>
2018-04-24 00:10:19 +0200

    modif time
    modif time

--- nebubot.py
+++ nebubot.py
@@ -52,86 +52,24 @@ async def check_event():
     margin = datetime.timedelta(days = 3)
     event_tab = []
     id = 0
-    k = 1
-    while (k == 1):
-        check = True
-        lol = 0
+       
+
+
+    while not bot.is_closed:
         for row in csv.reader(open('dates.csv', 'r')):
             date = datetime.datetime.strptime(row[1], '%Hh%M %d/%m/%Y')
             desc = row[2]
             obj = Event(date, desc)
             if (today <= obj.dt <= today + margin):
                 event_tab.append(obj)
-
-
-            if (today > obj.dt and check == True):
+            elif (today > obj.dt):
                 id = row[0]
-                k = 1
-                check = False
-                lol = 1
-        if (check == False):
-            dele(id)
-        if (lol == 0):
-            k = 0
-
-    while not bot.is_closed:
+                dele(id)
         for item in event_tab:
             await bot.send_message(discord.Object(id=general_channel_id), "@everyone, look at this reminder !"
 								   " You have a rendezvous planned at : " + item.str + "\n *" + item.desc + "*")
         await asyncio.sleep(86400) # task will run every day - 86400 sec
 
-#toDoDB = []
-
-#def channelDBManager(channel):
-#    channelExists = False
-#    for db in toDoDB:
-#        if db[0] == channel:
-#            channelExists = True
-#            currentDB = db[1]
-
-#    if not channelExists:
-#        toDoDB.append([channel, []])
-#        for db in toDoDB:
-#            if db[0] == channel:
-#                currentDB = db[1]
-#    try:
-#        return currentDB
-#    except:
-#        print("channeldbmanager fatal fail - exiting")
-#        exit()
-
-#def newToDo(desc, channel):
-#    db = channelDBManager(channel)
-#    status = True
-#    tempTodo = [desc, status]
-#    db.append(tempTodo)
-
-#def doneToDo(id, channel):
-#    db = channelDBManager(channel)
-#    i = 0
-#    success = False
-#    for todo in db:
-#        i += 1
-#        if i == id:
-#            success = True
-#            tempI = i - 1
-#            db.remove(db[tempI])
-#    return success
-
-#def listToDo(channel):
-#    db = channelDBManager(channel)
-#    id = 0
-#    returnList = []
-#    for todo in db:
-#        temptodo = []
-#        for i in todo:
-#            temptodo.append(i)
-#        id += 1
-#        temptodo.insert(0, id)
-#        returnList.append(temptodo)
-#    return returnList
-
-#bot = discord.Client()
 
 @bot.event
 async def on_ready():
@@ -145,82 +83,6 @@ async def ping(*args):
     """Ping Pong"""
     await bot.say(":ping_pong: Pong!")
 
-#@bot.event
-
-#async def on_message(message):
-
-#    channel = str(message.channel.id)
-
-#    command = str(message.content)
-
-#    if command.startswith("/"):
-
-#        print(toDoDB)
-
-#        if command.startswith("/add "):
-#            command = command.replace("/add ", "")
-#            if command == "":
-#                await bot.send_message(message.channel, ":x: No ToDo entered.")
-#                print(":x: No ToDo entered.")
-#            newToDo(command, channel)
-#            await bot.send_message(message.channel, ":white_check_mark: ToDo `" + command + "` added.")
-#            print(":white_check_mark: ToDo '" + command + "' added.")
-#        elif command == "/add":
-#            await bot.send_message(message.channel, ":x: Usage: `/add [TOD O]`")
-#            print(":x: Usage: `/add [TOD O]`")
-
-#        elif command == "/todo":
-#            list = listToDo(channel)
-#            printline = ""
-#            for todo in list:
-#                printline = printline + "**[" + str(todo[0]) + "]** - " + str(todo[1]) + "\n"
-#            if printline == "":
-#                printline = ":metal: **No ToDo's!** :metal:"
-#            else:
-#                printline = ":pencil: **ToDo's:** :pencil:\n\n" + printline
-#            await bot.send_message(message.channel, printline)
-#            print(printline)
-
-#        elif command.startswith("/done "):
-#            command = command.replace("/done ", "")
-#            try:
-#                command = int(command)
-#            except:
-#                await bot.send_message(message.channel, ':x: ID needs to be a number!')
-#                print(":x: ID needs to be a number!")
-#                return
-#            success = doneToDo(command, channel)
-#            if success:
-#                await bot.send_message(message.channel, ':white_check_mark: ToDo done.')
-#                print(":white_check_mark: ToDo deleted")
-#            else:
-#                await bot.send_message(message.channel, ':x: No ToDo with ID **' + str(command) + '**')
-#                print(':x: No ToDo with ID **' + str(command) + '**')
-#        elif command == "/done":
-#            await bot.send_message(message.channel, ":x: Usage: `/done [ID]`")
-#            print(":x: Usage: `/done [ID]`")
-
-#        elif command == "/help":
-#            printline = "'/todo' - Show the current todo list.\n'/add [TOD O]' - Add a new todo.\n'/done [ID]' - Mark a todo done (delete).\n'/help' - Show this menu.\nv1.0 by @xdavidhu"
-#            printline = ":question: **Help menu:** :question:\n" + "```" + printline + "```"
-#            await bot.send_message(message.channel, printline)
-#            print(printline)
-
-#        elif command.startswith("/"):
-#            printline = ":x: Command `" + command + "` not found. Type `/help` for the help menu."
-#            await bot.send_message(message.channel, printline)
-#            print(printline)
-
-#@bot.command()
-#async def new_rdv(*args):
-#    #i = -1
-#    d = datetime.datetime.strptime(' '.join(args), '%H:%M %d/%m/%Y')
-#    e = Event(d, "No description was given to this event.")
-#    num_lines = sum(1 for line in open('dates'))
-#    with open('dates', 'a') as file:
-#        file.write(str(num_lines) + ": " + e.str + ' *' + e.desc + '*\n')
-#    await bot.say("New Rendezvous is set at : " + e.str + "\n \"" + e.desc + "\"")
-#    file.close()
 
 @bot.command(aliases=['add_rdv'])
 async def new_rdv(*args):
